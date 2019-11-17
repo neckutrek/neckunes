6a3e70673a116a420c04434e3089e5cd441dd923
neckutrek <na>
2019-11-17 10:20:54 +0100

    Added main loop

--- neckunes/main.py
+++ neckunes/main.py
@@ -1,2 +1,20 @@
+import time
+
+def mainLoop(_fps, _func):
+   fps = _fps
+   run = True
+   start = time.time()
+   while run:
+      _func(time.time())
+      end = time.time()
+      if end-start < 1/fps:
+         t = 1/fps - (end-start)
+         time.sleep( t )
+      start = time.time()
+
 def main():
-   print("hello world2")
+   def p(t):
+      print("time: {}".format(t))
+   
+   mainLoop(4, p)
+   
